4a763bc0c9f38362527dad2aef3f2652234ebe69
chris <chris98822@gmail.com>
2020-05-11 12:56:43 -0600

    adding notes to ECS recipe file

--- ECS/main.tf
+++ ECS/main.tf
@@ -1,10 +1,18 @@
 data aws_caller_identity current {
 
+#Provision a Production Environment
 module fargate_prod {
-    source            = ./module
+    
+    #Environment Specifics:
     sdlc              = prod
+    container_cpu     = 198
+    container_memory  = 198
+    app_count         = 2
+
+    #Global Values
+    source            = ./module
     stage             = var.stage
-    image             = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.product}-prod:latest"
+    image             = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.product}-${var.sdlc}:latest"
     dd_image          = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-west-2.amazonaws.com/datadog:latest"
     project           = var.project
     product           = var.product
@@ -12,8 +20,7 @@ module fargate_prod {
     app_port          = var.app_port
     domain            = var.domain
     alternate_domain  = var.san_domain
-    app_count         = 2
-    deploy_type       = ECS
+    deploy_type       = var.deploy_type
     company           = var.company
     key_name          = var.key_name
     fargate_cpu       = var.fargate_cpu
@@ -23,6 +30,36 @@ module fargate_prod {
     dd_api_key        = var.dd_api_key
     asg_min           = var.asg_min
     asg_max           = var.asg_max
+}
+
+#Provision a Development Environment
+module fargate_dev {
+    
+    #Environment Specifics:
+    sdlc              = dev
     container_cpu     = 198
     container_memory  = 198
+    app_count         = 2
 
+    #Global Values
+    source            = ./module
+    stage             = var.stage
+    image             = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.product}-${var.sdlc}:latest"
+    dd_image          = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-west-2.amazonaws.com/datadog:latest"
+    project           = var.project
+    product           = var.product
+    aws_region        = us-west-2
+    app_port          = var.app_port
+    domain            = var.domain
+    alternate_domain  = var.san_domain
+    deploy_type       = var.deploy_type
+    company           = var.company
+    key_name          = var.key_name
+    fargate_cpu       = var.fargate_cpu
+    fargate_memory    = var.fargate_memory
+    app_image         = var.app_image
+    ecs_iam_role      = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
+    dd_api_key        = var.dd_api_key
+    asg_min           = var.asg_min
+    asg_max           = var.asg_max
+}
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Terraform Module Examples
 
 ## Summary
-The included folders showcase the logic and orginizational structure of a best-practice Terraform implimentation.
+The included folders showcase the logic and orginizational concepts of a best-practice Terraform implimentation.
 
 Contained within each resource folder is a main.tf file that gives an example of the recipe file used to call the module. 
 The module itself is contained with the module folder.
+
+
